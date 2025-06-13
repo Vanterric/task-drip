bc17f0026332f25479143830a846a5ac9d14ddc0
Vanterric <derrickgallegos@rocketmail.com>
2025-06-13 17:40:42 -0400

    Updated Testimonials

--- front-end/src/pages/login/testimonials.jsx
+++ front-end/src/pages/login/testimonials.jsx
@@ -1,18 +1,18 @@
 export const testimonials = [
-    /* {
+    {
     name: "Alice",
     role: "ADHD Coach & Specialist",
     quote:"No one can multitask. We just have to focus on getting something done. DewList’s simple, single-task design is perfect for neurodivergent minds."
-    }, */
+    },
   {
     name: "Davie",
     role: "CS Master's Student",
-    quote: "Overall, it's clean. I love it. I love the glowing hover, amazing animations - small details like that make a difference."
+    quote: "Overall, it's clean. I love the simplicity. Love the interactions. And, above all, I love the fun animations - small details like that make a difference."
   },
   {
     name: "Brandon",
     role: "Marketing at DewList",
-    quote: "Been Pro for months. Never used the AI. I’m too stubborn. But one task at a time? That’s the kind of micromanagement I can handle."
+    quote: "I've been hesitant to adopt all this new AI stuff, but DewList's AI feature is almost invisible and makes adding tasks a breeze! The app looks beautiful too."
   },
   {
     name: "Derrick",
